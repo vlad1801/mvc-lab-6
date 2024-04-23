f1ae1b3ac7852018b1749db81884127a23ec66af
Dazai <53435069+vlad1801@users.noreply.github.com>
2024-04-23 18:59:06 +0300

    ..

--- zad1/controllers/error.js
+++
@@ -1,7 +0,0 @@
-const getNotFoundPage = (_, res) => {
-    res.status(404).render('not-found', { title: 'Page Not Found' });
-  }
-  
-  module.exports = {
-    getNotFoundPage,
-  }
--- zad1/routes/error.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { getNotFoundPage } = require('../controllers/error');
-
-router.use('*', getNotFoundPage);
-
-module.exports = router;
